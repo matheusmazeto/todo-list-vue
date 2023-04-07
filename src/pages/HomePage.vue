3ab9900fa812eff56900caf05d2932ea1c588c0b
Matheus Mazeto <nb27582@novabase.pt>
2023-04-07 16:19:59 +0100

    feat: todo mvp

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,22 +1,37 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useTodoStore } from '@/store/todo'
+
+const todoStore = useTodoStore()
+
+const inputValue = ref('')
+
+function addTodo() {
+  const text = inputValue.value
+  inputValue.value = ''
+
+  if (text.trim() !== '')
+    todoStore.addTodo({ text })
+}
 </script>
 
 <template>
   <div id="app">
-    <h1>To-Do List</h1>
-    <!-- <to-do-form @todo-added="addToDo"></to-do-form>
-    <h2 id="list-summary" ref="listSummary" tabindex="-1">{{ listSummary }}</h2>
-    <ul aria-labelledby="list-summary" class="stack-large">
-      <li v-for="item in ToDoItems" :key="item.id">
-        <to-do-item
-          :label="item.label"
-          :done="item.done"
-          :id="item.id"
-          @checkbox-changed="updateDoneStatus(item.id)"
-          @item-deleted="deleteToDo(item.id)"
-          @item-edited="editToDo(item.id, $event)">
-        </to-do-item>
+    <h1>Todos</h1>
+
+    <form @submit.prevent="addTodo">
+      <label for="add">Add Todo</label>
+      <input id="add" v-model="inputValue" type="text" name="add">
+      <button type="submit">
+        Add
+      </button>
+    </form>
+
+    <ul v-for="todo in todoStore.todos" :key="todo.id">
+      <li>
+        <input :id="todo.id" :checked="todo.isCompleted" type="checkbox" name="isCompleted" @change="todoStore.toggleTodo({ id: todo.id })">
+        <label :for="todo.id" :class="todo.isCompleted ? 'completed' : 'incompleted'">{{ todo.text }}</label>
       </li>
-    </ul> -->
+    </ul>
   </div>
 </template>
