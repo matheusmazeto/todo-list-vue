0ccf996c214e1850ab3e464c274403d6434d9e7b
Matheus Mazeto <nb27582@novabase.pt>
2023-04-07 14:11:59 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+
+<template>
+ <div id="app">
+    <h1>To-Do List</h1>
+    <!-- <to-do-form @todo-added="addToDo"></to-do-form>
+    <h2 id="list-summary" ref="listSummary" tabindex="-1">{{ listSummary }}</h2>
+    <ul aria-labelledby="list-summary" class="stack-large">
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item
+          :label="item.label"
+          :done="item.done"
+          :id="item.id"
+          @checkbox-changed="updateDoneStatus(item.id)"
+          @item-deleted="deleteToDo(item.id)"
+          @item-edited="editToDo(item.id, $event)">
+        </to-do-item>
+      </li>
+    </ul> -->
+  </div>
+</template>
