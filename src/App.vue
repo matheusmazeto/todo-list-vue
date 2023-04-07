2013f5a77132995b2e1047b024519ab3c27a0236
Matheus Mazeto <nb27582@novabase.pt>
2023-04-07 14:56:22 +0100

    chore: add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
- <div id="app">
-    <h1>To-Do List</h1>
-    <!-- <to-do-form @todo-added="addToDo"></to-do-form>
-    <h2 id="list-summary" ref="listSummary" tabindex="-1">{{ listSummary }}</h2>
-    <ul aria-labelledby="list-summary" class="stack-large">
-      <li v-for="item in ToDoItems" :key="item.id">
-        <to-do-item
-          :label="item.label"
-          :done="item.done"
-          :id="item.id"
-          @checkbox-changed="updateDoneStatus(item.id)"
-          @item-deleted="deleteToDo(item.id)"
-          @item-edited="editToDo(item.id, $event)">
-        </to-do-item>
-      </li>
-    </ul> -->
-  </div>
+  <RouterView />
 </template>
